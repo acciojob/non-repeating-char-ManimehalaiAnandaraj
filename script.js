9847948933131346c33a6ff02262151d9a1fee78
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-21 11:52:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(!str)
+	{
+		return null;
+	}
+	const char_counts={};
+	for(let char of str)
+		{
+			char_counts[char] = (char_counts[char] || 0) + 1;
+		}
+	for(let char of str)
+		{
+		if(char_counts[char] === 1)
+		{
+			return char;
+		}
+		}
+	return null; 
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
